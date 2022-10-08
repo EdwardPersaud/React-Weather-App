e0863c4c7bc1b8eeca6feb6937688c2aa4b820a5
EdwardPersaud <epersaud1998@gmail.com>
2022-10-07 21:45:11 -0400

    GitHub project

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f1b41a1d0emsh35b7254fa67d617p103723jsndd0061ed7d47',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
