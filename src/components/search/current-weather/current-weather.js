b573f10f76639645be66cf575705e0eb1984d501
EdwardPersaud <epersaud1998@gmail.com>
2022-10-09 08:50:01 -0400

    Added some styling for the weather component. Note: flexbox not positioning the text and icon as expected

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -4,8 +4,10 @@ const CurrentWeather = () => {
     return (
         <div className = "weather">
             <div className = "top">
-                <p className = "city">New York</p>
-                <p className = "weather-description">Sunny</p>
+                <div>
+                    <p className = "city">New York</p>
+                    <p className = "weather-description">Sunny</p>
+                </div>
             </div>
             <img alt="weather" className="weather-icon" src="icons/01d.png"/>
         </div>
